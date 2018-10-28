346916f8cb2fde3a79f49a87544de78cca158be3
Cody Receno <creceno@sumologic.com>
2018-10-28 05:27:48 -0700

    arduino stuff

---
+++ tc-arduino/test.js
@@ -0,0 +1,15 @@
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/cu.usbmodem1441');
+
+port.write('1', function(err) {
+  if (err) {
+    return console.log('Error on write: ', err.message);
+  }
+  console.log('message written');
+});
+ 
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
+
